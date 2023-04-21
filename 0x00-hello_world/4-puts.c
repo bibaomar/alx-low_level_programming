325469729316ed059f9b0d821ef14bbb434e41bc
bibaomar <bibanaiem07@gmail.com>
2023-04-20 19:15:51 -0700

    this is my commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry pint
+ *
+ *Description: A C peogram that prints with put function
+ *
+ *Return: Always 0(success) 
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
+
